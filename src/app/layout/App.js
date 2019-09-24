012a37bef00fcfe653c6fc8b81a9178ef9d25bde
koylyakandriy <koylyakandriy@gmail.com>
2019-09-24 17:21:07 +0300

    add dependencies

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
 
 class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<h1>Re-vents</h1>
+				<button className='ui icon button'>
+					<i className='smile icon' />
+					CSS
+				</button>
+
+				<Button icon='smile' content='React Button' />
 			</div>
 		);
 	}
